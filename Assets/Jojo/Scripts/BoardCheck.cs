3ff0c01aca99327e9b97d90c7137d94979859259
GunzRuls <143023790+GunzRuls@users.noreply.github.com>
2023-10-20 23:06:41 -0400

    Finished board

--- Assets/Jojo/Scripts/BoardCheck.cs
+++ Assets/Jojo/Scripts/BoardCheck.cs
@@ -4,21 +4,30 @@ using UnityEngine;
 
 public class BoardCheck : MonoBehaviour
 {
-    public Vector3 middlePoint;
     Vector3 input;
-    public GameObject[] inputSlots; 
+    public GameObject[] SpawnPoints;
+    public GameObject test;
+    int count;
 
     private void Start()
     {
-        middlePoint = GetComponent<Transform>().localPosition;
+        count = 0;
+        for (int i = 0; i < SpawnPoints.Length; i++)
+        {
+            if (SpawnPoints[i].transform.childCount == 0)
+            {
+
+            }
+        }
     }
 
     public void OnTriggerEnter(Collider other)
     {
-        print("colliding is working");
         if (other.GetComponent<InputEnum>() != null)
         {
-            
+            if (count == SpawnPoints.Length) return;
+            other.transform.position = SpawnPoints[count].transform.position;
+            count++;
         }
     }
 }
--- Assets/Jojo/Scripts/InputEnum.cs
+++ Assets/Jojo/Scripts/InputEnum.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class InputEnum : MonoBehaviour
 {
-    public Transform move;
     public enum ListOfInputs
     {
         If,
         equals
-
-
     }
     public ListOfInputs input;
+    private void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(Vector3.forward * 4);
+    }
 }
